6410b5288fb376d0edf96cb1d8d39d959865c674
Foxycoder <r.kalimulin@jetmail.cc>
2024-06-24 11:53:23 +0400

    first build

---
+++ src/components/Landing.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="landing">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
