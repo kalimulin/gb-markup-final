5546c9f04e290199c32105cff43503b1b065dc21
Foxycoder <r.kalimulin@jetmail.cc>
2024-06-24 15:46:56 +0400

    mobile sections

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -28,11 +28,26 @@
         <div class="banner__subtitle" v-html="TEXTS.banner_subtitle"></div>
       </div>
     </div>
+    <div class="sections">
+      <div class="container">
+        <div
+          v-for="{ str1, str2, classSuffix } in SECTIONS"
+          :key="str2"
+          class="sections__item section"
+          :class="`section_${classSuffix}`"
+        >
+          <div class="section__text">
+            <div class="section__text1">{{ str1 }}</div>
+            <div class="section__text2">{{ str2 }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { TEXTS } from './constants'
+import { TEXTS, SECTIONS } from './constants'
 
 export default {
   name: 'LandingComponent',
@@ -41,6 +56,7 @@ export default {
   },
   created () {
     this.TEXTS = TEXTS
+    this.SECTIONS = SECTIONS
   }
 }
 </script>
--- src/components/constants.js
+++ src/components/constants.js
@@ -2,3 +2,26 @@ export const TEXTS = {
   banner_title: 'THE BRAND',
   banner_subtitle: 'OF LUXERIOUS <span>FASHION</span>'
 }
+
+export const SECTIONS = [
+  {
+    str1: '30% OFF',
+    str2: 'FOR WOMEN',
+    classSuffix: 'women'
+  },
+  {
+    str1: 'HOT DEAL',
+    str2: 'FOR MEN',
+    classSuffix: 'men'
+  },
+  {
+    str1: 'NEW ARRIVALS',
+    str2: 'FOR KIDS',
+    classSuffix: 'kids'
+  },
+  {
+    str1: 'LUXIROUS & TRENDY',
+    str2: 'ACCESORIES',
+    classSuffix: 'accesories'
+  }
+]
