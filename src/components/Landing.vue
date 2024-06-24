48bfc9dc95a0c85d1c521c3bc4820b38d0ce9f63
Foxycoder <r.kalimulin@jetmail.cc>
2024-06-24 13:38:47 +0400

    mobile header

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="landing">
+    <header class="landing__header header">
+      <div class="container header__wrap">
+        <div class="header__logo">
+          <img
+            src="@/assets/img/logo.png"
+            alt="logo"
+          >
+        </div>
+        <div class="header__search">
+          <img
+            src="@/assets/img/icon-search.svg"
+            alt="search"
+          >
+        </div>
+        <div class="header__menu">
+          <img
+            src="@/assets/img/icon-hamburger.svg"
+            alt="menu"
+          >
+        </div>
+      </div>
+    </header>
   </div>
 </template>
 
@@ -12,5 +34,4 @@ export default {
 }
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss" src="./style.scss"></style>
