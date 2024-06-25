7071f1122cb205dd25274d7a140529a8a76d2cb0
Foxycoder <r.kalimulin@jetmail.cc>
2024-06-25 17:47:04 +0400

    router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,85 @@
 <template>
-  <Landing />
+  <div class="landing">
+    <header class="landing__header header">
+      <div class="container header__wrap">
+        <div class="header__logo">
+          <img
+            src="@/assets/img/logo.png"
+            alt="logo"
+          >
+        </div>
+        <div class="header__search">
+          <img
+            src="@/assets/img/icon-search.svg"
+            alt="search"
+          >
+        </div>
+        <div class="header__menu">
+          <img
+            src="@/assets/img/icon-hamburger.svg"
+            alt="menu"
+          >
+        </div>
+        <div class="header__user">
+          <img
+            src="@/assets/img/icon-user.svg"
+            alt="user"
+          >
+        </div>
+        <div class="header__cart">
+          <img
+            src="@/assets/img/icon-cart.svg"
+            alt="user"
+          >
+        </div>
+      </div>
+    </header>
+    <router-view/>
+    <footer class="footer">
+      <div class="container">
+        <div class="footer__socials">
+          <div class="footer__social">
+            <img
+              src="@/assets/img/socials/facebook.svg"
+              class="footer__social-img"
+              alt="social"
+            >
+          </div>
+          <div class="footer__social">
+            <img
+              src="@/assets/img/socials/intstagram.svg"
+              class="footer__social-img"
+              alt="social"
+            >
+          </div>
+          <div class="footer__social">
+            <img
+              src="@/assets/img/socials/pinterest.svg"
+              class="footer__social-img"
+              alt="social"
+            >
+          </div>
+          <div class="footer__social">
+            <img
+              src="@/assets/img/socials/twitter.svg"
+              class="footer__social-img"
+              alt="social"
+            >
+          </div>
+        </div>
+        <div class="footer__copyright">{{ TEXTS.copyright }}</div>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
-import Landing from './components/Landing.vue'
+import { TEXTS } from '@/components/constants'
 
 export default {
   name: 'App',
-  components: {
-    Landing
+  created () {
+    this.TEXTS = TEXTS
   }
 }
 </script>
--- src/components/HomePage/index.vue
+++ src/components/HomePage/index.vue
@@ -1,39 +1,5 @@
 <template>
-  <div class="landing">
-    <header class="landing__header header">
-      <div class="container header__wrap">
-        <div class="header__logo">
-          <img
-            src="@/assets/img/logo.png"
-            alt="logo"
-          >
-        </div>
-        <div class="header__search">
-          <img
-            src="@/assets/img/icon-search.svg"
-            alt="search"
-          >
-        </div>
-        <div class="header__menu">
-          <img
-            src="@/assets/img/icon-hamburger.svg"
-            alt="menu"
-          >
-        </div>
-        <div class="header__user">
-          <img
-            src="@/assets/img/icon-user.svg"
-            alt="user"
-          >
-        </div>
-        <div class="header__cart">
-          <img
-            src="@/assets/img/icon-cart.svg"
-            alt="user"
-          >
-        </div>
-      </div>
-    </header>
+  <div class="home-page">
     <section class="banner">
       <div class="banner__left">
         <img
@@ -140,52 +106,14 @@
         </div>
       </div>
     </section>
-    <footer class="footer">
-      <div class="container">
-        <div class="footer__socials">
-          <div class="footer__social">
-            <img
-              src="@/assets/img/socials/facebook.svg"
-              class="footer__social-img"
-              alt="social"
-            >
-          </div>
-          <div class="footer__social">
-            <img
-              src="@/assets/img/socials/intstagram.svg"
-              class="footer__social-img"
-              alt="social"
-            >
-          </div>
-          <div class="footer__social">
-            <img
-              src="@/assets/img/socials/pinterest.svg"
-              class="footer__social-img"
-              alt="social"
-            >
-          </div>
-          <div class="footer__social">
-            <img
-              src="@/assets/img/socials/twitter.svg"
-              class="footer__social-img"
-              alt="social"
-            >
-          </div>
-        </div>
-        <div class="footer__copyright">{{ TEXTS.copyright }}</div>
-      </div>
-    </footer>
   </div>
 </template>
 
 <script>
-import { TEXTS, SECTIONS, FEATURED_ITEMS, CAPABILITIES } from './constants'
+import { CAPABILITIES, FEATURED_ITEMS, SECTIONS, TEXTS } from '@/components/constants'
 
 export default {
-  name: 'LandingComponent',
-  props: {
-    msg: String
-  },
+  name: 'HomeView',
   created () {
     this.TEXTS = TEXTS
     this.SECTIONS = SECTIONS
