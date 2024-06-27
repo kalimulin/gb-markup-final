e0dedafca5043ded24bff36eed78401e58d798e5
Foxycoder <r.kalimulin@jetmail.cc>
2024-06-27 11:00:49 +0400

    remove vw

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,6 @@
 <template>
   <div class="landing">
-    <header class="landing__header header">
-      <div class="container header__wrap">
-        <div class="header__logo">
-          <router-link to="/">
-            <img
-              src="@/assets/img/logo.png"
-              alt="logo"
-            >
-          </router-link>
-        </div>
-        <div class="header__search">
-          <img
-            src="@/assets/img/icon-search.svg"
-            alt="search"
-          >
-        </div>
-        <div class="header__menu">
-          <img
-            src="@/assets/img/icon-hamburger.svg"
-            alt="menu"
-            @click="openMenu = !openMenu"
-          >
-        </div>
-        <div class="header__user">
-          <img
-            src="@/assets/img/icon-user.svg"
-            alt="user"
-          >
-        </div>
-        <div class="header__cart">
-          <img
-            src="@/assets/img/icon-cart.svg"
-            alt="user"
-          >
-        </div>
-        <nav
-          v-if="openMenu"
-          class="header__popup-menu popup-menu"
-        >
-          <div
-            class="popup-menu__close"
-            @click="openMenu = false"
-          >
-            <img
-              src="@/assets/img/icon-close.svg"
-              alt="user"
-            >
-          </div>
-          <div class="popup-menu__title">{{ TEXTS.popupMenuTitle }}</div>
-          <ul class="popup-menu__items">
-            <li
-              v-for="({ title, submenu }, number) in POPUP_MENU"
-              :key="number"
-              class="popup-menu__menu-item"
-            >
-              <div class="popup-menu__menu-item-title">
-                {{ title }}
-              </div>
-              <ul
-                v-if="submenu"
-                class="popup-menu__submenu"
-              >
-                <li
-                  v-for="({ title, path }, number) in submenu"
-                  :key="number"
-                  class="popup-menu__submenu-item"
-                  @click="openMenu = false"
-                >
-                  <router-link :to="path" >{{ title }}</router-link>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+    <Header :texts="TEXTS" :popup-menu="POPUP_MENU" />
     <router-view/>
     <Footer :text-copyright="TEXTS.copyright" />
   </div>
@@ -83,11 +8,13 @@
 
 <script>
 import { TEXTS, POPUP_MENU } from '@/components/constants'
+import Header from '@/components/HeaderComponent'
 import Footer from '@/components/FooterComponent'
 
 export default {
   name: 'App',
   components: {
+    Header,
     Footer
   },
   data: () => {
---
+++ src/components/HeaderComponent/index.vue
@@ -0,0 +1,106 @@
+<template>
+  <header class="landing__header header">
+    <div class="container header__wrap">
+      <div class="header__logo">
+        <router-link to="/">
+          <img
+            src="@/assets/img/logo.png"
+            alt="logo"
+          >
+        </router-link>
+      </div>
+      <div class="header__search">
+        <img
+          src="@/assets/img/icon-search.svg"
+          alt="search"
+        >
+      </div>
+      <div class="header__menu">
+        <img
+          src="@/assets/img/icon-hamburger.svg"
+          alt="menu"
+          @click="openMenu = !openMenu"
+        >
+      </div>
+      <div class="header__user">
+        <img
+          src="@/assets/img/icon-user.svg"
+          alt="user"
+        >
+      </div>
+      <div class="header__cart">
+        <img
+          src="@/assets/img/icon-cart.svg"
+          alt="user"
+        >
+      </div>
+      <div
+        v-if="openMenu"
+        class="header__overlay"
+        @click="openMenu = false"
+      ></div>
+      <nav
+        v-if="openMenu"
+        class="header__popup-menu popup-menu"
+      >
+        <div
+          class="popup-menu__close"
+          @click="openMenu = false"
+        >
+          <img
+            src="@/assets/img/icon-close.svg"
+            alt="user"
+          >
+        </div>
+        <div class="popup-menu__title">{{ texts.popupMenuTitle }}</div>
+        <ul class="popup-menu__items">
+          <li
+            v-for="({ title, submenu }, number) in popupMenu"
+            :key="number"
+            class="popup-menu__menu-item"
+          >
+            <div class="popup-menu__menu-item-title">
+              {{ title }}
+            </div>
+            <ul
+              v-if="submenu"
+              class="popup-menu__submenu"
+            >
+              <li
+                v-for="({ title, path }, number) in submenu"
+                :key="number"
+                class="popup-menu__submenu-item"
+                @click="openMenu = false"
+              >
+                <router-link :to="path" >{{ title }}</router-link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+  props: {
+    texts: {
+      type: Object,
+      default: () => {}
+    },
+    popupMenu: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data: () => {
+    return {
+      openMenu: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./style.scss"></style>
