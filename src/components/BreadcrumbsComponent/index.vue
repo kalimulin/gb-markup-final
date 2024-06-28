48dfe082a00d63d34db3cdce14386baf958df129
Foxycoder <r.kalimulin@jetmail.cc>
2024-06-28 10:32:39 +0400

    hovers

---
+++ src/components/BreadcrumbsComponent/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="breadcrumbs">
+    <div class="breadcrumbs__title">NEW ARRIVALS</div>
+    <div class="breadcrumbs__path">Cart page</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BreadcrumbsComponent'
+}
+</script>
+
+<style lang="scss" src="./style.scss"></style>
