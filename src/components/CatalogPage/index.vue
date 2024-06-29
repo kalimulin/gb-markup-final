c29f3bb86a2da1aef365f150255a5b54acd614c6
Foxycoder <r.kalimulin@jetmail.cc>
2024-06-29 22:27:30 +0400

    breadcrumbs

--- src/components/CatalogPage/index.vue
+++ src/components/CatalogPage/index.vue
@@ -4,6 +4,9 @@
     <div class="container">
       <div class="catalog-page__products">
         <product-list :product-list="PRODUCTS" />
+        <div class="catalog-page__pagination">
+          <Pagination />
+        </div>
       </div>
     </div>
     <Capabilities />
@@ -15,10 +18,11 @@ import { PRODUCTS } from '@/components/constants'
 import ProductList from '@/components/ProductListComponent/index.vue'
 import Capabilities from '@/components/CapabilitiesComponent/index.vue'
 import Breadcrumbs from '@/components/BreadcrumbsComponent/index.vue'
+import Pagination from '@/components/PaginationComponent/index.vue'
 
 export default {
   name: 'HomeView',
-  components: { Breadcrumbs, Capabilities, ProductList },
+  components: { Pagination, Breadcrumbs, Capabilities, ProductList },
   created () {
     this.PRODUCTS = PRODUCTS
   }
---
+++ src/components/PaginationComponent/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="pagination">
+    <div class="pagination__prev">
+      <img
+        src="@/assets/img/chevron-left.svg"
+        class="pagination__arrow"
+      >
+    </div>
+    <div
+      v-for="(page, number) in [1,2,3,4,5]"
+      :key="number"
+      class="pagination__item"
+      :class="{'pagination__item_active': page === 1}"
+    >
+      {{ page }}
+    </div>
+    <div class="pagination__dots">.....</div>
+    <div class="pagination__last">20</div>
+    <div class="pagination__next">
+      <img
+        src="@/assets/img/chevron-left.svg"
+        class="pagination__arrow pagination__arrow_right"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaginationComponent'
+}
+</script>
+
+<style lang="scss" src="./style.scss"></style>
